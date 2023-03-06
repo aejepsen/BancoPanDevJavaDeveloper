e3bebc972ccbc4ce0e43291002fe9e4e8563a700
allan <aejepsen@yahoo.com.br>
2023-03-05 21:14:06 -0300

    ok

--- basecamp/basecamp/src/aula2/Quadrilatero.java
+++ basecamp/basecamp/src/aula2/Quadrilatero.java
@@ -7,19 +7,19 @@ public class Quadrilatero {
     System.out.println("Área do quadrado:" + lado * lado);
 }
 
-public static void area(double lado1, double lado2) {
+  public static void area(double lado1, double lado2) {
 
-    System.out.println("Área do retângulo:" + lado1 * lado2);
-}
+      System.out.println("Área do retângulo:" + lado1 * lado2);
+  }
 
-public static void area(double baseMaior, double baseMenor, double altura) {
+  public static void area(double baseMaior, double baseMenor, double altura) {
 
-    System.out.println("Área do trapézio:" + ((baseMaior+baseMenor)*altura) / 2);
-}
+      System.out.println("Área do trapézio:" + ((baseMaior+baseMenor)*altura) / 2);
+  }
 
-public static void area(float diagonal1, float diagonal2) {
+  public static void area(float diagonal1, float diagonal2) {
 
-    System.out.println("Área do losango:" + (diagonal1 * diagonal2)/2);
-}
-  
+      System.out.println("Área do losango:" + (diagonal1 * diagonal2)/2);
+  }
+    
 }
